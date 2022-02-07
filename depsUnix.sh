6f8b42c50d5e160b226f26a5e1a3095ef0d69ed1
Sergey Sharpov <sharpov.peaceit@gmail.com>
2022-02-07 18:35:18 +0200

    linux build update (directory separator change)

--- depsUnix.sh
+++ depsUnix.sh
@@ -1,17 +1,17 @@
 # Build GLTF format
-cd .\3rdparty\collada2gltf\GLTF
+cd ./3rdparty/collada2gltf/GLTF
 mkdir build
-cd .\build
+cd ./build
 cmake ../ -G "Unix Makefiles"
 make
-rm ".\..\..\..\..\lib\libGLTF.a"
-rm ".\..\..\..\..\lib\libdraco.a"
-rm ".\..\..\..\..\lib\libdracoenc.a"
-rm ".\..\..\..\..\lib\libdracodec.a"
-cp ".\libGLTF.a" ".\..\..\..\..\lib\libGLTF.a"
-cp ".\dependencies\draco\libdraco.a" ".\..\..\..\..\lib\libdraco.a"
-cp ".\dependencies\draco\libdracoenc.a" ".\..\..\..\..\lib\libdracoenc.a"
-cp ".\dependencies\draco\libdracodec.a" ".\..\..\..\..\lib\libdracodec.a"
+rm "./../../../../lib/libGLTF.a"
+rm "./../../../../lib/libdraco.a"
+rm "./../../../../lib/libdracoenc.a"
+rm "./../../../../lib/libdracodec.a"
+cp "./libGLTF.a" "./../../../../lib/libGLTF.a"
+cp "./dependencies/draco/libdraco.a" "./../../../../lib/libdraco.a"
+cp "./dependencies/draco/libdracoenc.a" "./../../../../lib/libdracoenc.a"
+cp "./dependencies/draco/libdracodec.a" "./../../../../lib/libdracodec.a"
 make clean
-cd .\..
-rm -rf ".\build"
+cd ./..
+rm -rf "./build"
