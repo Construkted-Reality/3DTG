baec6facd61e35a9fa0a11e2bd203411b9b0dca5
Sergey Sharpov <sharpov.peaceit@gmail.com>
2022-02-07 19:07:39 +0200

    separators in build file

--- depsUnix.sh
+++ depsUnix.sh
@@ -6,14 +6,14 @@ mkdir -p build
 cd ./build
 cmake ../ -G "Unix Makefiles"
 make
-rm -f ".\..\..\..\..\lib\libGLTF.a"
-rm -f ".\..\..\..\..\lib\libdraco.a"
-rm -f ".\..\..\..\..\lib\libdracoenc.a"
-rm -f ".\..\..\..\..\lib\libdracodec.a"
-cp "./libGLTF.a" ".\..\..\..\..\lib\libGLTF.a"
-cp "./dependencies/draco/libdraco.a" ".\..\..\..\..\lib\libdraco.a"
-cp "./dependencies/draco/libdracoenc.a" ".\..\..\..\..\lib\libdracoenc.a"
-cp "./dependencies/draco/libdracodec.a" ".\..\..\..\..\lib\libdracodec.a"
+rm -f "./../../../../lib/libGLTF.a"
+rm -f "./../../../../lib/libdraco.a"
+rm -f "./../../../../lib/libdracoenc.a"
+rm -f "./../../../../lib/libdracodec.a"
+cp "./libGLTF.a" "./../../../../lib/libGLTF.a"
+cp "./dependencies/draco/libdraco.a" "./../../../../lib/libdraco.a"
+cp "./dependencies/draco/libdracoenc.a" "./../../../../lib/libdracoenc.a"
+cp "./dependencies/draco/libdracodec.a" "./../../../../lib/libdracodec.a"
 make clean
 cd ./..
 rm -rf "./build"
