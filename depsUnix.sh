39921de5e01a1aebae10052c8eb8301558d50c39
Sergey Sharpov <sharpov.peaceit@gmail.com>
2022-02-07 18:43:38 +0200

    linux bash update

--- depsUnix.sh
+++ depsUnix.sh
@@ -1,13 +1,14 @@
+#!/bin/bash
 # Build GLTF format
 cd ./3rdparty/collada2gltf/GLTF
 mkdir build
 cd ./build
 cmake ../ -G "Unix Makefiles"
 make
-rm ".\..\..\..\..\lib\libGLTF.a"
-rm ".\..\..\..\..\lib\libdraco.a"
-rm ".\..\..\..\..\lib\libdracoenc.a"
-rm ".\..\..\..\..\lib\libdracodec.a"
+rm -f ".\..\..\..\..\lib\libGLTF.a"
+rm -f ".\..\..\..\..\lib\libdraco.a"
+rm -f ".\..\..\..\..\lib\libdracoenc.a"
+rm -f ".\..\..\..\..\lib\libdracodec.a"
 cp ".\libGLTF.a" ".\..\..\..\..\lib\libGLTF.a"
 cp ".\dependencies\draco\libdraco.a" ".\..\..\..\..\lib\libdraco.a"
 cp ".\dependencies\draco\libdracoenc.a" ".\..\..\..\..\lib\libdracoenc.a"
