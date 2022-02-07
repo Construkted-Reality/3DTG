107d5bed5a3b229277aa1a4f76246e6a7a163311
Sergey Sharpov <sharpov.peaceit@gmail.com>
2022-02-07 18:57:50 +0200

    linux separators fix

--- depsUnix.sh
+++ depsUnix.sh
@@ -2,7 +2,7 @@
 mkdir -p ./lib
 # Build GLTF format
 cd ./3rdparty/collada2gltf/GLTF
-mkdir build
+mkdir -p build
 cd ./build
 cmake ../ -G "Unix Makefiles"
 make
@@ -11,10 +11,10 @@ rm -f ".\..\..\..\..\lib\libdraco.a"
 rm -f ".\..\..\..\..\lib\libdracoenc.a"
 rm -f ".\..\..\..\..\lib\libdracodec.a"
 cp "./libGLTF.a" ".\..\..\..\..\lib\libGLTF.a"
-cp "./dependencies\draco\libdraco.a" ".\..\..\..\..\lib\libdraco.a"
-cp "./dependencies\draco\libdracoenc.a" ".\..\..\..\..\lib\libdracoenc.a"
-cp "./dependencies\draco\libdracodec.a" ".\..\..\..\..\lib\libdracodec.a"
+cp "./dependencies/draco/libdraco.a" ".\..\..\..\..\lib\libdraco.a"
+cp "./dependencies/draco/libdracoenc.a" ".\..\..\..\..\lib\libdracoenc.a"
+cp "./dependencies/draco/libdracodec.a" ".\..\..\..\..\lib\libdracodec.a"
 make clean
 cd ./..
-rm -rf ".\build"
+rm -rf "./build"
 cd ./../../..
