cb64aa0ec5e2266b6969b9d1e249da6c9ca545e6
Sergey Sharpov <sharpov.peaceit@gmail.com>
2022-02-07 18:53:52 +0200

    linux build folders separator

--- depsUnix.sh
+++ depsUnix.sh
@@ -9,10 +9,11 @@ rm -f ".\..\..\..\..\lib\libGLTF.a"
 rm -f ".\..\..\..\..\lib\libdraco.a"
 rm -f ".\..\..\..\..\lib\libdracoenc.a"
 rm -f ".\..\..\..\..\lib\libdracodec.a"
-cp ".\libGLTF.a" ".\..\..\..\..\lib\libGLTF.a"
-cp ".\dependencies\draco\libdraco.a" ".\..\..\..\..\lib\libdraco.a"
-cp ".\dependencies\draco\libdracoenc.a" ".\..\..\..\..\lib\libdracoenc.a"
-cp ".\dependencies\draco\libdracodec.a" ".\..\..\..\..\lib\libdracodec.a"
+cp "./libGLTF.a" ".\..\..\..\..\lib\libGLTF.a"
+cp "./dependencies\draco\libdraco.a" ".\..\..\..\..\lib\libdraco.a"
+cp "./dependencies\draco\libdracoenc.a" ".\..\..\..\..\lib\libdracoenc.a"
+cp "./dependencies\draco\libdracodec.a" ".\..\..\..\..\lib\libdracodec.a"
 make clean
 cd ./..
 rm -rf ".\build"
+cd ./../../..
